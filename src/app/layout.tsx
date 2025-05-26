39cbd1845db96a800f233f1e416e9e350c511e88
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2025-05-26 22:54:16 +0530

    clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Auth App',
-  description: 'Next.js Authentication App',
+  title: 'Career Secure',
+  description: 'Career secure internal app',
 };
 
 export default function RootLayout({
