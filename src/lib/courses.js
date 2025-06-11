42f724c6275531b44b3827e5059b04954524d41d
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2025-06-11 12:12:05 +0530

    added university data

---
+++ src/lib/courses.js
@@ -0,0 +1,467 @@
+const variable = {
+  CUIDOL: [],
+  CUOL: [
+    {
+      course: "BA",
+      specialization: "Journalism & Mass Communication",
+    },
+    {
+      course: "BBA",
+      specialization: "Banking and Finance",
+    },
+    {
+      course: "BBA",
+      specialization: "ACCA",
+    },
+    {
+      course: "BBA",
+      specialization: "General",
+    },
+    {
+      course: "BBA",
+      specialization: "Business Analytics",
+    },
+    {
+      course: "BBA",
+      specialization: "Entrepreneurship",
+    },
+    {
+      course: "BBA",
+      specialization: "Human Resource Management (HRM)",
+    },
+    {
+      course: "BBA",
+      specialization: "Marketing",
+    },
+    {
+      course: "BBA",
+      specialization: "International Business",
+    },
+    {
+      course: "BBA",
+      specialization: "Family Business",
+    },
+    {
+      course: "BBA",
+      specialization: "Fin-Tech",
+    },
+    {
+      course: "BBA",
+      specialization: "Retail and E-Commerce",
+    },
+    {
+      course: "BBA",
+      specialization: "Artificial Intelligence",
+    },
+    {
+      course: "BBA",
+      specialization: "HealthCare Management",
+    },
+    {
+      course: "BBA",
+      specialization: "Logistics and Supply Chain Management",
+    },
+    {
+      course: "BBA",
+      specialization: "Foreign Exchange Management",
+    },
+    {
+      course: "BBA",
+      specialization: "Travel & Tourism Management",
+    },
+    {
+      course: "BBA",
+      specialization: "Event Management",
+    },
+    {
+      course: "BBA",
+      specialization: "Digital Marketing",
+    },
+    {
+      course: "BCA",
+      specialization: "General",
+    },
+    {
+      course: "MA",
+      specialization: "English",
+    },
+    {
+      course: "MA",
+      specialization: "Journalism & Mass Communication",
+    },
+    {
+      course: "MA",
+      specialization: "Economics",
+    },
+    {
+      course: "MBA",
+      specialization: "Business Analytics",
+    },
+    {
+      course: "MBA",
+      specialization: "Marketing",
+    },
+    {
+      course: "MBA",
+      specialization: "HRM",
+    },
+    {
+      course: "MBA",
+      specialization: "IB (International Business)",
+    },
+    {
+      course: "MBA",
+      specialization: "Entrepreneurship",
+    },
+    {
+      course: "MBA",
+      specialization: "Finance",
+    },
+    {
+      course: "MBA",
+      specialization: "Disaster Management",
+    },
+    {
+      course: "MBA",
+      specialization: "IT (Information Technology)",
+    },
+    {
+      course: "MBA",
+      specialization: "Logistics & Supply Chain Management",
+    },
+    {
+      course: "MBA",
+      specialization: "Retail Management",
+    },
+    {
+      course: "MBA",
+      specialization: "Operations Management",
+    },
+    {
+      course: "MBA",
+      specialization: "Banking & Insurance",
+    },
+    {
+      course: "MBA",
+      specialization: "Hospital Management",
+    },
+    {
+      course: "MBA",
+      specialization: "Event Management",
+    },
+    {
+      course: "MBA",
+      specialization: "Travel & Tourism Management",
+    },
+    {
+      course: "MBA",
+      specialization: "Airlines & Airport Management",
+    },
+    {
+      course: "MBA",
+      specialization: "Digital Marketing",
+    },
+    {
+      course: "MCA",
+      specialization: "Artificial Intelligence and Machine Learning",
+    },
+    {
+      course: "MCA",
+      specialization: "Data Analytics",
+    },
+    {
+      course: "MCA",
+      specialization: "Full Stack Development",
+    },
+    {
+      course: "MCA",
+      specialization: "Cloud Computing",
+    },
+    {
+      course: "MCA",
+      specialization: "General",
+    },
+    {
+      course: "MSC",
+      specialization: "Data Science",
+    },
+    {
+      course: "MSC",
+      specialization: "Mathematics",
+    },
+  ],
+  MUJ: [
+    { course: "BBA", specialization: "General" },
+    { course: "BCA", specialization: "General" },
+    { course: "BCom", specialization: "General" },
+    { course: "MA", specialization: "Economics" },
+    { course: "MA", specialization: "JMC" },
+    { course: "MBA", specialization: "Finance" },
+    { course: "MBA", specialization: "Marketing" },
+    { course: "MBA", specialization: "HRM" },
+    { course: "MBA", specialization: "Analytics & Data Science" },
+    { course: "MBA", specialization: "Retail Management" },
+    { course: "MBA", specialization: "BFSI" },
+    { course: "MBA", specialization: "IT & FinTech" },
+    { course: "MBA", specialization: "Operations Management" },
+    { course: "MCA", specialization: "General" },
+    { course: "MCom", specialization: "General" },
+  ],
+  NMIMS: [
+    { course: "BBA", specialization: "Business Analytics" },
+    { course: "BBA", specialization: "Marketing and Finance" },
+    { course: "BCOM", specialization: "General" },
+    {
+      course: "MBA Distance",
+      specialization: "Operations & Data Sciences Management",
+    },
+    { course: "MBA Distance", specialization: "Marketing Management" },
+    { course: "MBA Distance", specialization: "Human Resource Management" },
+    { course: "MBA Distance", specialization: "Financial Management" },
+    { course: "MBA Distance", specialization: "Business Management" },
+    { course: "MBA Distance", specialization: "General" },
+    {
+      course: "MBA WX",
+      specialization: "Operations & Supply Chain Management",
+    },
+    { course: "MBA WX", specialization: "Marketing Management" },
+    { course: "MBA WX", specialization: "Leadership & Strategy" },
+    { course: "MBA WX", specialization: "Digital Marketing" },
+    { course: "MBA WX", specialization: "Applied Finance" },
+    { course: "MBA WX", specialization: "General" },
+  ],
+  LPU: [
+    {
+      course: "BA",
+      specialization: "General",
+    },
+    {
+      course: "BBA",
+      specialization: "General",
+    },
+    {
+      course: "BCA",
+      specialization: "General",
+    },
+    {
+      course: "DBA",
+      specialization: "General",
+    },
+    {
+      course: "DCA",
+      specialization: "General",
+    },
+    {
+      course: "M.Com",
+      specialization: "General",
+    },
+    {
+      course: "M.Sc",
+      specialization: "Economics",
+    },
+    {
+      course: "M.Sc",
+      specialization: "Mathematics",
+    },
+    {
+      course: "MA",
+      specialization: "Political Science",
+    },
+    {
+      course: "MA",
+      specialization: "English",
+    },
+    {
+      course: "MA",
+      specialization: "History",
+    },
+    {
+      course: "MA",
+      specialization: "Sociology",
+    },
+    {
+      course: "MBA",
+      specialization: "Human Resource Management",
+    },
+    {
+      course: "MBA",
+      specialization: "International Business",
+    },
+    {
+      course: "MBA",
+      specialization: "Finance",
+    },
+    {
+      course: "MBA",
+      specialization: "Marketing",
+    },
+    {
+      course: "MBA",
+      specialization: "Operations Management",
+    },
+    {
+      course: "MBA",
+      specialization: "Business Analytics",
+    },
+    {
+      course: "MBA",
+      specialization: "Digital Marketing",
+    },
+    {
+      course: "MBA",
+      specialization: "Data Science",
+    },
+    {
+      course: "MBA",
+      specialization: "Information Technology",
+    },
+    {
+      course: "MBA",
+      specialization: "Dual Specialization",
+    },
+    {
+      course: "MCA",
+      specialization: "AR/VR (Game Development)",
+    },
+    {
+      course: "MCA",
+      specialization: "Full Stack Web Development",
+    },
+    {
+      course: "MCA",
+      specialization: "Cybersecurity",
+    },
+    {
+      course: "MCA",
+      specialization: "Data Science",
+    },
+    {
+      course: "MCA",
+      specialization: "Machine Learning & AI",
+    },
+    {
+      course: "MBA",
+      specialization: "Hospital and Healthcare Management",
+    },
+    {
+      course: "MBA",
+      specialization: "Logistics and Supply Chain Management",
+    },
+    {
+      course: "MBA",
+      specialization: "Banking and Financial Services",
+    },
+  ],
+  JU: [],
+  AU: [],
+  HU: [],
+  MR: [],
+  DPU: [],
+  UPES: [],
+  "LV-Tech": [],
+  "SVU-Tech": [],
+  "SVU-CMS": [],
+  "SVU-Agriculture": [],
+  SDU: [],
+  UpGrad: [],
+  VU: [],
+  UU: [],
+  LPUODL: [],
+  MMU: [],
+  GLA: [],
+  DMIHER: [],
+  Edureka: [],
+  SCDOE: [],
+  Nulearn: [],
+  MITSDE: [],
+  VIT: [],
+  SMU: [],
+  Amrita: [],
+  "MUOL-Retail": [],
+  Zell: [],
+  Andhra: [],
+  DYPATIL: [],
+  KUK: [],
+  SGVU: [],
+  BDU: [],
+  MGR: [],
+  VGU: [],
+  MAHE: [],
+  Imarticus: [],
+  Sharda: [],
+  "SU-Tech": [],
+  Veranda: [],
+  MZU: [],
+  AMU: [],
+  JHD: [],
+  Coursera: [],
+  TimesPro: [],
+  PU: [],
+  Futurense: [],
+  Shobhit: [],
+  Birchwood: [],
+  IMI: [],
+  SRM: [],
+  NIU: [],
+  EIMT: [],
+  Galgotias: [
+    {
+      course: "M.Com",
+      specialization: "General",
+    },
+    {
+      course: "MA",
+      specialization: "Economics",
+    },
+    {
+      course: "MA",
+      specialization: "English",
+    },
+    {
+      course: "MBA",
+      specialization: "Marketing",
+    },
+    {
+      course: "MBA",
+      specialization: "Finance",
+    },
+    {
+      course: "MBA",
+      specialization: "HR",
+    },
+    {
+      course: "MBA",
+      specialization: "Business Analytics and Information Technology",
+    },
+    {
+      course: "MBA",
+      specialization: "Operations",
+    },
+    {
+      course: "MBA",
+      specialization: "Healthcare Management",
+    },
+    {
+      course: "MBA",
+      specialization: "International Business",
+    },
+    {
+      course: "MCA",
+      specialization: "Data Analytics",
+    },
+    {
+      course: "MCA",
+      specialization: "Computer Network & Cyber Security",
+    },
+    {
+      course: "MCA",
+      specialization: "Cloud Computing",
+    },
+    {
+      course: "MCA",
+      specialization: "Web Designing & Security",
+    },
+  ],
+};
+
+export default variable;
